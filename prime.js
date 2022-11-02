5554a847c26a4e05fbeb205d8239f50af736b101
Andy18j <115467259+Andy18j@users.noreply.github.com>
2022-11-02 18:25:23 +0530

    prime code

---
+++ prime.js
@@ -0,0 +1,19 @@
+function checkPrime(num){
+let count=0;
+   
+for(let i=0; i<=num; i++){
+if(num%i==0){
+
+}
+}
+if(count==0){
+    return true;
+}
+  return false;
+}
+let ans = checkPrime(17);
+if (ans==true){
+  console.log(prime)
+}else { console.log(not prime)
+      }
+
